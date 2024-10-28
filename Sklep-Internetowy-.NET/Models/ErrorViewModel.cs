92ad39bcf3fcf1d9a1f1e17d96e29c8d70b94c4f
JakubL <kubas307@gameil.com>
2024-10-28 16:17:46 +0100

    Initial commit

---
+++ Sklep-Internetowy-.NET/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Sklep_Internetowy_.NET.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
